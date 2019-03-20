d453ef90d476ce13b2b8e48d83f201bd12744eb8
lenasinitcyna <helen.sinitcyna@gmail.com>
2019-03-20 07:59:34 -0600

    store likes in localstorage

--- src/js/index.js
+++ src/js/index.js
@@ -148,10 +148,6 @@ elements.shopping.addEventListener('click', e => {
 /** 
  * LIKE CONTROLLER 
  * */
-// TESTING
-state.likes = new Likes();
-likesView.toggleLikeMenu(state.likes.getNumLikes());
-
 const controlLike = () => {
     if(!state.likes){
         state.likes = new Likes();
@@ -186,6 +182,22 @@ const controlLike = () => {
     }
     likesView.toggleLikeMenu(state.likes.getNumLikes());
 };
+
+// restore likes recipes on page load
+window.addEventListener('load', () => {
+    state.likes = new Likes();
+
+    // restore likes
+    state.likes.readStorage();
+
+    // toggle like menu button
+    likesView.toggleLikeMenu(state.likes.getNumLikes());
+
+    // render the existing likes
+    state.likes.likes.forEach(like => likesView.renderLike(like));
+    
+       
+});
 
 
 /** 
--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -1,3 +1,5 @@
+import { throws } from "assert";
+
 export default class Likes {
     constructor(){
         this.likes = [];
@@ -6,12 +8,20 @@ export default class Likes {
     addLike(id, title, author, img){
         const like = {id, title, author, img};
         this.likes.push(like);
+
+        // persist data in local storage
+        this.persistData();
+
         return like;
     }
 
     deleteLike(id){
         const index = this.likes.findIndex(el => el.id == id);
         this.likes.splice(index, 1);
+
+         // persist data in local storage
+         this.persistData();
+
     }
 
     isLiked(id){
@@ -20,6 +30,18 @@ export default class Likes {
 
     getNumLikes(){
         return this.likes.length;
+    }
+
+    persistData(){
+        localStorage.setItem('likes', JSON.stringify(this.likes));
+    }
+
+    readStorage(){
+        const storage = JSON.parse(localStorage.getItem('likes'));
+        // restoring from the local storage
+        if(storage){
+            this.likes = storage;
+        }
     }
 
 }
